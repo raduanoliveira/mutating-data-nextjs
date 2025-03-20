984e42170a47a3cee3330adb9c7a18387da02a0a
Raduan de Oliveira <raduan.oliveira@ufrn.br>
2025-03-20 13:50:23 -0300

    final da sessao

--- actions/posts.js
+++ actions/posts.js
@@ -1,7 +1,8 @@
 "use server"
 
 import { uploadImage } from '@/lib/cloudinary';
-import { storePost } from '@/lib/posts';
+import { storePost, updatePostLikeStatus } from '@/lib/posts';
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 export async function createPost(prevState, formData) {
@@ -34,5 +35,11 @@ export async function createPost(prevState, formData) {
         content,
         userId: 1
     })
+    revalidatePath('/','layout')
     redirect('/feed')
+}
+
+export async function togglePostLikeStatus(postId){
+    await updatePostLikeStatus(postId, 2)
+    revalidatePath('/','layout')
 }
